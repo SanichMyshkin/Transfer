05f07fa3dd6ac54c5d11335371f51c1e724782c4
Александр Мышкин <sir.myshkin@gmail.com>
2025-11-07 10:13:50 +0300

    add ad-group scripts

--- allocate-service/jenkins/script.groovy
+++ allocate-service/jenkins/script.groovy
@@ -1,31 +1,16 @@
+import hudson.model.User
+import jenkins.security.LastGrantedAuthoritiesProperty
 import groovy.json.JsonOutput
 
-def strategy = Jenkins.instance.getAuthorizationStrategy()
-def loader = Jenkins.instance.pluginManager.uberClassLoader
-
-// пробуем старый пакет (Michelin)
-def RoleType = loader.loadClass('com.michelin.cio.hudson.plugins.rolestrategy.RoleType')
-def RoleBasedAuthorizationStrategy = loader.loadClass('com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy')
-
-if (!RoleBasedAuthorizationStrategy.isInstance(strategy)) {
-    return JsonOutput.toJson([error: "Role-Strategy plugin есть, но не используется как текущая авторизация"])
+def users = User.getAll().collect { u ->
+    def authProp = u.getProperty(LastGrantedAuthoritiesProperty)
+    def authorities = authProp?.getAuthorities() ?: []
+    [
+        id: u.id,
+        fullName: u.fullName,
+        email: u.getProperty(hudson.tasks.Mailer.UserProperty)?.address ?: "",
+        ad_groups: authorities
+    ]
 }
 
-def collectRoles = { roleType ->
-    def roleMap = strategy.getRoleMap(roleType)
-    roleMap.getRoles().collect { r ->
-        [
-            name: r.getName(),
-            permissions: r.getPermissions()*.id,
-            sids: roleMap.getSidsForRole(r.getName())
-        ]
-    }
-}
-
-def roles = [
-    global : collectRoles(RoleType.Global),
-    project: collectRoles(RoleType.Project),
-    folder : collectRoles(RoleType.Item)
-]
-
-JsonOutput.toJson(roles)
+JsonOutput.toJson([users: users, total: users.size()])
