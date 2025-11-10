81b533bf3d017449ec60c6b870af34d0185b6b08
Александр Мышкин <sir.myshkin@gmail.com>
2025-11-10 17:14:05 +0300

    add jenkins docs

--- allocate-service/jenkins/jenkins_groovy.py
+++ allocate-service/jenkins/jenkins_groovy.py
@@ -1,3 +1,4 @@
+# jenkins_groovy.py
 import requests
 import json
 
--- allocate-service/jenkins/jenkins_scripts.py
+++ allocate-service/jenkins/jenkins_scripts.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+# jenkins_scripts.py
 # --- Получение пользователей ---
 SCRIPT_USERS = """
 import hudson.model.User
